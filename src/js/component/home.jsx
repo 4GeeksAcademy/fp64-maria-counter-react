68c81ef75f8c73fb2c638e59c74b54e80fe8fcc6
MaríaSF <158471273+DexpinBe@users.noreply.github.com>
2024-04-20 14:51:14 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
-
-//include images into your bundle
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [counter, setCounter] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect( () => {
+    const theCounter = setInterval(() => {
+      setCounter(counter => counter + 1);
+    }, 1000);
+
+    setIntervalId(theCounter);
+
+    return () => {
+      clearInterval(theCounter);
+    };
+  }, []);
+
+  const stopInterval = () => {
+    clearInterval(intervalId);
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">Hola contador!</h1>
+      <p>
+        <img src={rigoImage} alt="Rigo" />
+      </p>
+      <p>Contador hasta que lo pares: {counter}</p>
+      <button className="btn btn-success" onClick={stopInterval}>¡¡Basta de contar!!</button>
+    </div>
+  );
 };
 
 export default Home;
+
